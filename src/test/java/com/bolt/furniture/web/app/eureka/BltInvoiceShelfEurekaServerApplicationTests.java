fe25e7c57ebce0e2a0e75fba91c4646e2bac354a
DESKTOP-AO62CC3\Joaco <joaco103@hotmail.com>
2023-01-19 14:45:17 -0500

    primer commit

---
+++ src/test/java/com/bolt/furniture/web/app/eureka/BltInvoiceShelfEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolt.furniture.web.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BltInvoiceShelfEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
